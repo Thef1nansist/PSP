f3737cfaa42bf2561bb8b2fde3097800d612d8d4
Thef1nansist <vladsavhchenko994@gmail.com>
2021-09-22 16:01:35 +0300

    I love C++(no)

---
+++ ClientT/ClientT.cpp
@@ -0,0 +1,98 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Winsock2.h"
+#include <string>
+#include "WS2tcpip.h"
+#pragma comment(lib, "WS2_32.lib")
+
+using namespace std;
+
+string GetErrorMsgText(int code) {
+    string msgText;
+    switch (code) {
+    case WSAEINTR: msgText = "Function interrupted";
+        break;
+    case WSAEACCES: msgText = "Permission denied";
+        break;
+    case WSAEFAULT: msgText = "Wrong address";
+        break;
+    case WSASYSCALLFAILURE: msgText = "System call abort";
+        break;
+    default: msgText = "***ERROR***";
+        break;
+    }
+    return msgText;
+}
+
+
+string SetErrorMsgText(string msgText, int code) { return  msgText + GetErrorMsgText(code); }
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SOCKET cC;
+    WSADATA ws;
+    SOCKADDR_IN serv;
+
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &ws) != 0) {
+            throw SetErrorMsgText("Startup: ", WSAGetLastError());
+        }
+
+        if (INVALID_SOCKET == (cC = socket(AF_INET, SOCK_STREAM, NULL))) {
+
+            throw SetErrorMsgText("Socket: ", WSAGetLastError());
+        }
+
+        serv.sin_family = AF_INET;
+        serv.sin_port = htons(2000);
+        inet_pton(AF_INET, "127.0.0.1", &(serv.sin_addr));
+        if (SOCKET_ERROR == connect(cC, (sockaddr*)&serv, sizeof(serv))) {
+            throw SetErrorMsgText("Connect: ", WSAGetLastError());
+        }
+
+        char ibuf[50], obuf[50] = "Hello from Client";
+        int libuf = 0, lobuf = 0;
+        char b[] = "Hello from Client";
+
+        int count;
+        cout << "Enter count of messages" << endl;
+        cin >> count;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (SOCKET_ERROR == send(cC, obuf, sizeof(obuf), NULL))
+            {
+                cout << "Send : " << GetLastError() << endl;;
+            }
+
+            if (SOCKET_ERROR == recv(cC, ibuf, sizeof(ibuf), NULL))
+            {
+                cout << "Recv : " << GetLastError() << endl;
+            }
+            cout << (i + 1) << "Serv: " << ibuf << endl;
+        }
+
+        if (SOCKET_ERROR == send(cC, "CLOSE", sizeof("CLOSE"), NULL)) {
+            cout << "send exit: " << GetLastError() << endl;
+        }
+
+        if (closesocket(cC) == SOCKET_ERROR) {
+            throw SetErrorMsgText("CloseSocket", WSAGetLastError());
+        }
+
+        if (WSACleanup() == SOCKET_ERROR) {
+            throw SetErrorMsgText("Cleanup: ", WSAGetLastError());
+        }
+
+    }
+    catch (string errMsg)
+    {
+        cout << endl  << errMsg;
+    }
+    
+    system("pause");
+    return 0;
+}
---
+++ Labs2/ServerT.cpp
@@ -0,0 +1,118 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include "Winsock2.h"
+#pragma comment(lib, "WS2_32.lib")
+#include <ctime>
+
+using namespace std;
+
+string GetErrorMsgText(int code) {
+    string msgText;
+    switch (code) {
+    case WSAEINTR: msgText = "Function interrupted";
+        break;
+    case WSAEACCES: msgText = "Permission denied";
+        break;
+    case WSAEFAULT: msgText = "Wrong address";
+        break;
+    case WSASYSCALLFAILURE: msgText = "System call abort";
+        break;
+    default: msgText = "***ERROR***";
+        break;
+    }
+    return msgText;
+}
+
+
+string SetErrorMsgText(string msgText, int code) { return  msgText + GetErrorMsgText(code); }
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Server is started\n";
+    cout << "Server is waiting connections\n";
+    int count = 0;
+    char countBuf[50];
+    char oBuf[50] = "ECHO: Hi from client";
+
+    WSADATA ws;
+    SOCKET s;
+    SOCKET c;
+
+    try
+    {
+        if (FAILED(WSAStartup(MAKEWORD(1, 1), &ws))) {
+            cout << "Socket: " << WSAGetLastError() << endl;
+        }
+        if (INVALID_SOCKET == (s = socket(AF_INET, SOCK_STREAM, 0))) {
+            cout << "Socket: " << WSAGetLastError() << endl;
+        }
+
+
+        sockaddr_in s_adr;
+        sockaddr_in c_adr;
+        {
+            s_adr.sin_port = htons(2000);
+            s_adr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+            s_adr.sin_family = AF_INET;
+        }
+        
+        if (SOCKET_ERROR == (bind(s, (LPSOCKADDR)&s_adr, sizeof(s_adr)))) {
+            cout << "Bind: " << WSAGetLastError() << endl;
+        }
+        if (SOCKET_ERROR == listen(s, 2)) {
+            cout << "Listen: " << WSAGetLastError() << endl;
+        }
+
+        for (;;) {
+            int lcInt = sizeof(c_adr);
+            if (INVALID_SOCKET == (c = accept(s, (sockaddr*)&c_adr, &lcInt)))
+            {
+                cout << "Accept: " << WSAGetLastError() << endl;
+            }
+
+            cout << "    Client connect\n";
+            cout << "Addres client:       " << inet_ntoa(c_adr.sin_addr) << " : " << htons(c_adr.sin_port) << endl << endl << endl << endl << endl << endl;
+
+            for (;;) {
+                if (SOCKET_ERROR == recv(c, countBuf, sizeof(countBuf), NULL)) {
+                    cout << "Recv: " << WSAGetLastError() << endl;
+                    break;
+                }
+
+                cout << count << "Client: " << countBuf << endl;
+                count++;
+
+                if (!strcmp(countBuf, "CLOSE")) {
+                    break;
+                }
+                if (SOCKET_ERROR == send(c, oBuf, strlen(oBuf) + 1, NULL)) {
+                    cout << "Send: " << WSAGetLastError() << endl;
+                    break;
+                }
+                count = 0;
+                cout << "\t\tClient Disconnect: \n";
+            }
+            if (closesocket(c) == SOCKET_ERROR)
+            {
+                throw SetErrorMsgText("closesocket: ", WSAGetLastError());
+            }
+
+            if (WSACleanup() == SOCKET_ERROR)
+            {
+                throw SetErrorMsgText("Cleanup: ", WSAGetLastError());
+            }
+        }
+    }
+
+    catch (string errMassage)
+    {
+        cout << endl << errMassage;
+    }
+
+    return 0;
+}
